d8d3649c51c42b54fe7b07c943ccc9faa13d6caf
Sergei Gavrusev <sgauruseu@scnsoft.com>
2013-08-15 18:16:36 +0300

    fix compilation error

--- src/main/java/com/enonic/autotests/pages/v4/adminconsole/content/BaseImprtWizardPage.java
+++ src/main/java/com/enonic/autotests/pages/v4/adminconsole/content/BaseImprtWizardPage.java
@@ -1,9 +1,16 @@
 package com.enonic.autotests.pages.v4.adminconsole.content;
 
+import com.enonic.autotests.TestSession;
 import com.enonic.autotests.pages.v4.adminconsole.AbstractAdminConsolePage;
 
 public class BaseImprtWizardPage extends AbstractAdminConsolePage
 {
+
+	public BaseImprtWizardPage( TestSession session )
+	{
+		super(session);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void waituntilPageLoaded(long timeout)
